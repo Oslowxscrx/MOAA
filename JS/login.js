ae73cb8c51d1adb53566fcb9be7f6fea8ca45ba5
steveen Ordoñez <hsc.ordonez@yavirac.edu.ec>
2022-08-01 21:56:27 -0500

    feat:login

--- JS/login.js
+++ JS/login.js
@@ -1,52 +1,11 @@
-var usuariosRegistrados = [];
-function ingresarSistema(usuario, clave, sesionIniciada = false) {
-    console.log(usuario);
-    for (let i = 0; i < usuariosRegistrados.length; i++) {
-      if (
-        usuario == usuariosRegistrados[i].usuario &&
-        clave == usuariosRegistrados[i].clave
-      ) {
-        usuariosRegistrados[i].sesionIniciada = sesionIniciada;
-        localStorage.setItem(
-          "usuarioLogueado",
-          JSON.stringify(usuariosRegistrados[i])
-        );
-        redireccionar();
-        return;
-      }
-    }
-
-    alert("el usuario o contrseña son incorrectos")
-    return;
-  }
+  function iniciarSesion(){
+    var usuario, clave;
+    usuario = document.getElementById("usuario").value;
+    clave = document.getElementById("clave").value;
   
-  function verificarInicioSesion() {
-    let usuarioLogueado = JSON.parse(localStorage.getItem("usuarioLogueado"));
-    if (usuarioLogueado != null && usuarioLogueado.sesionIniciada == true) {
-     
-
+    if(usuario == "171245" && clave =="123" ){
+        window.location = "./dashboard.html";
+    }else{
+        alert("El usuario o contraseña es incorrecto");
     }
   }
-  
-  function redireccionar() {
-    window.location = "../usuarios/dashboard.html";
-  }
-  
-  function obtenerUsuarios() {
-    usuariosRegistrados = JSON.parse(localStorage.getItem("usuariosRegistrados"));
-    if (usuariosRegistrados == null  || usuariosRegistrados.length == 0 ) {
-      usuariosRegistrados = [
-        {
-          usuario: "MOAA",
-          clave: "123",
-          nombre: "juan",
-          apellido: "perez",
-          sesionIniciada: false,
-        },
-      ];
-    }
-  }
-
-  obtenerUsuarios();
-  verificarInicioSesion();
-
