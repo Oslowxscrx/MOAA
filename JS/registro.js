cf64096bce5f8d12db325ef25cfb0157a02026b2
hscordonez1810 <hsc.ordonez@yavirac.edu.ec>
2022-08-15 22:08:44 -0500

    fix:solucionar bugs

--- JS/registro.js
+++ JS/registro.js
@@ -1,43 +1,45 @@
-function validacion(){
-    if(formulario.nombre.value==null){
-        alert("llene campo")
-    }
-    else{
-        validarNombre();
+function validacion() {
+
+
+    validarNombre();
     validarApellido();
     validarfecha()
     window.location = "./dashboard.html";
-    }
-    
+
+
 }
-function validarNombre(){
-    if (formulario.nombre.value==0){
+function validarNombre() {
+    if (formulario.nombre.value == 0) {
         alert("Falta nombre");
     }
 }
-function validarApellido(){
-    if (formulario.apellido.value==0){
+function validarApellido() {
+    if (formulario.apellido.value == 0) {
         alert("Falta apellido");
     }
 }
 function validarfecha() {
-    if(formulario.fecha.value==0){
+    if (formulario.fecha.value == 0) {
         alert("ingrese fecha")
     }
-    }
+}
 function validarSexo() {
-    if(formulario1.sexo[0].checked==true || formulario1.sexo[1].checked==true){
+    if (formulario1.sexo[0].checked == true || formulario1.sexo[1].checked == true) {
 
     }
-    else{
+    else {
         alert("escoja su sexo");
     }
 }
-function validarCondiciones(){
-    if(formulario1.terminos.checked==true){
+function validarDireccion() {
+    window.location = "./dashboard.html";
 
+}
+function validarCondiciones() {
+    if (formulario1.terminos.checked == true) {
+        window.location = "./dashboard.html";
     }
-    else{
+    else {
         alert("acepte terminos")
     }
 
