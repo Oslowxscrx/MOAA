d40f78e7e4c431791de6449cb5ac7452fe2671fb
hscordonez1810 <hsc.ordonez@yavirac.edu.ec>
2022-08-15 22:00:56 -0500

    fix:solucionar bugs

--- JS/login.js
+++ JS/login.js
@@ -1,20 +1,11 @@
-  function iniciarSesion(){
-    var usuario, clave;
-    usuario = document.getElementById("usuario").value;
-    clave = document.getElementById("clave").value;
-  
-<<<<<<< HEAD
-    if(usuario == "moaastyle@gmail.com" && clave =="123" ){
-      redireccionar();   
-=======
-    if(usuario == "moaastyles@gmail.com" && clave =="123" ){
-        window.location = "./dashboard.html";
->>>>>>> S-2
-    }else{
-        alert("El usuario o contraseña es incorrecto");
-    }
+function iniciarSesion(){
+  var usuario, clave;
+  usuario = document.getElementById("usuario").value;
+  clave = document.getElementById("clave").value;
+
+  if(usuario == "moaastyles@gmail.com" && clave =="123" ){
+      window.location = "./dashboard.html";
+  }else{
+      alert("El usuario o contraseña es incorrecto");
   }
-  function redireccionar(){
-    window.location = "./pp.html";
-    console.log("entra");
-  }
+}
